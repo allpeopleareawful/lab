fc86fcbfd3fb9a714fe97e0f39477f68a7a76c49
isitmuse <egor.kozlov.03@mail.ru>
2022-05-15 20:41:18 +0300

    Working at 7th

--- secondSem/kp7/kp7.cpp
+++ secondSem/kp7/kp7.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 void printShortMatrix(int *M, float *A, int cntLn, int cntNz) {
-    printf("Short matrix\n");
+    printf("\nShort matrix\n");
     for (int i = 0; i < cntLn; ++i) {
         printf("%3d", M[i]);
     }
@@ -10,6 +10,7 @@ void printShortMatrix(int *M, float *A, int cntLn, int cntNz) {
     for (int i = 0; i < 2 * cntNz; ++i) {
         printf("%2.1f ", A[i]);
     }
+    printf("\n");
 }
 
 void printFullMatrix(float **matrix, int n, int m) {
@@ -20,10 +21,13 @@ void printFullMatrix(float **matrix, int n, int m) {
         }
         printf("\n");
     }
+    printf("\n");
 }
 
 void task(float **matrix, int n, int m) {
     int maxNz = 0, nz = 0, colInd = 0, preCol = 0;
+    float multiply = 1;
+
     for (int j = 0; j < m; ++j) {
         for (int i = 0; i < n; ++i) {
             if (matrix[i][j] != 0) {
@@ -39,37 +43,45 @@ void task(float **matrix, int n, int m) {
         }
         nz = 0;
     }
-    nz = 1;
     for (int i = 0; i < n; ++i) {
-        nz *= matrix[i][preCol];
+        multiply *= matrix[i][preCol];
     }
-    printf("\nTask answer:\nmultiply: %d\ncolumn: %d\n", nz, preCol);
+    printf("\nTask answer:\nmultiply: %2.2f\ncolumn: %d\n", multiply, preCol);
 }
 
 void shortToFull(float **matrix, int *M, float *A, int cntLn, int cntNz) {
-    int start, end, ln = 0;
-    for (ln = 0; ln < cntLn - 1; ++ln) {
-        start = M[ln];
-        end = M[ln + 1];
-        for (int j = start; j < end; ++j) {
-            matrix[ln][int(A[j])] = A[j + 1];
+    int start, end, cur, i, j, lastInd;
+    for (i = 0; i < cntLn; ++i) {
+        if (M[i] == 0) continue;
+        start = M[i];
+        lastInd = i - (1 * M[i] == 0);
+        for (cur = i + 1; (cur < cntLn - 1) && (M[cur] == 0); ++cur);
+        end = M[cur];
+        for (j = start - 1; j < end - 1; ++j) {
+            matrix[i][int(A[j])] = A[j + 1];
             ++j;
         }
     }
     //last pass
-    for (int j = end; j < 2 * cntNz; ++j) {
-        matrix[ln][int(A[j])] = A[j + 1];
+    for (j = M[lastInd]-1; j < 2 * cntNz; ++j) {
+        matrix[lastInd][int(A[j])] = A[j + 1];
         ++j;
     }
 }
 
-void fullToShort(float **matrix, int *M, float *A, int n, int m) {
+void fullToShort(float **matrix, int *M, float *A, int cntNz, int n, int m) {
+    for (int i = 0; i < n; ++i) {
+        M[i] = 0;
+    }
+    for (int i = 0; i < cntNz * 2; ++i) {
+        A[i] = 0;
+    }
     int cnt = 0, zero = 0;
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
             if (matrix[i][j] != 0) {
                 if (!zero) {
-                    M[i] = cnt;
+                    M[i] = cnt + 1;
                     zero = 1;
                 }
                 A[cnt++] = j;
@@ -109,7 +121,10 @@ int main() {
     printShortMatrix(M, A, n, cntNz);
     shortToFull(matrix, M, A, n, cntNz);
     printFullMatrix(matrix, n, m);
+    fullToShort(matrix, M, A, cntNz, n, m);
+    printShortMatrix(M, A, n, cntNz);
 
     task(matrix, n, m);
+
     return 0;
 }
